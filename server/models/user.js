ea3cf2911b9b99b1bf48c3938e798847fe20dc66
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-06-14 17:46:42 +0200

    Add POST /users route and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -25,7 +27,25 @@ const UserSchema = new mongoose.Schema({
     }]
 });
 
-// !!! const ??
+UserSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'name']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+    let user = this;
+    const access = 'auth';
+    const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 let app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['name', 'password']);
+
+    let user = new User(body);
+
+    user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server up on port ${port}`);
