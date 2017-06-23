a7cc57accda929e68ba39731aa9eb2028e387162
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-06-23 19:54:35 +0200

    Fix PollSchema

--- server/models/poll.js
+++ server/models/poll.js
@@ -36,7 +36,7 @@ PollSchema.methods.toJSON = function() {
     let poll = this;
     let pollObject = poll.toObject();
 
-    return _.pick(pollObject, ['_id', 'question', 'options', 'totalVotes', 'voters']);
+    return _.pick(pollObject, ['_id', 'question', 'options', 'totalVotes', 'voters', 'creatorName']);
 }
 
 
